426ae4c0d0e8ec316e771a5185326758d8b59b6d
Clark Phillips <clark.r.phillips@gmail.com>
2015-09-13 19:28:03 -0500

    rename

--- run.sh
+++ run.sh
@@ -4,9 +4,7 @@
 
 echo " ***** backward-forward-chaining ***********************************"
 
-#g++ *.h *.cpp -o chaining-gui
-#g++ *.h *.cpp -o chaining-gui `pkg-config --cflags --libs libglade-2.0`
-g++ -o chaining-gui *.h *.cpp $(pkg-config gtk+-2.0 --cflags) $(pkg-config gtk+-2.0 --libs) $(pkg-config --cflags libglade-2.0) $(pkg-config --libs libglade-2.0)
+g++ -o chaining-gui *.h *.cc $(pkg-config gtk+-2.0 --cflags) $(pkg-config gtk+-2.0 --libs) $(pkg-config --cflags libglade-2.0) $(pkg-config --libs libglade-2.0)
 compiled_status=$?
 
 if [[ $compiled_status -eq 0 ]]
